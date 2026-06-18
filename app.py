972239ea42ea3c9e74b66641381909311e12da18
Ricardo-Meraz <rmeraz08022005@gmail.com>
2026-06-17 23:31:06 -0600

    Agregar app Flask

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "¡Hola, Mundo!"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=8080)
